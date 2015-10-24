9300082f68369b849ef023bdace83b786f46a8ae
En-ning <playpool513@gmail.com>
2015-10-24 21:48:44 +0800

    view: admin/accounts#new create

--- app/controllers/admin/accounts_controller.rb
+++ app/controllers/admin/accounts_controller.rb
@@ -8,4 +8,23 @@ class Admin::AccountsController < ApplicationController
     @account = Account.find(params[:id])
   end
 
+  def new
+    @account = Account.new
+  end
+
+  def create
+    @account = Account.new(account_params)
+    if @account.save
+      redirect_to admin_accounts_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def account_params
+    params.require(:account).permit(:name, :currency, :exchange_rate, :balance)
+  end
+
 end
