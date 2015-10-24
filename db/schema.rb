b8e71da88ce46800c9ceff6741aaf3447999d574
En-ning <playpool513@gmail.com>
2015-10-24 21:38:09 +0800

    $ rails g model account name currency exchange_rate:decimal balance:decimal

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20151024133013) do
+
+  create_table "accounts", force: :cascade do |t|
+    t.string   "name",          limit: 255
+    t.string   "currency",      limit: 255,                          default: "TWD"
+    t.decimal  "exchange_rate",             precision: 20, scale: 5, default: 1.0
+    t.decimal  "balance",                   precision: 20, scale: 5, default: 0.0
+    t.datetime "created_at",                                                         null: false
+    t.datetime "updated_at",                                                         null: false
+  end
 
 end
