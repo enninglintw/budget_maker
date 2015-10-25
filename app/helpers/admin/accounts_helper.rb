e63bf2fb8968a3c7eb92f307c1fd510fd45fc245
En-ning <playpool513@gmail.com>
2015-10-25 15:58:49 +0800

    refactor with helper

--- app/helpers/admin/accounts_helper.rb
+++ app/helpers/admin/accounts_helper.rb
@@ -1,2 +1,24 @@
 module Admin::AccountsHelper
+
+  def render_currency(account)
+    account.currency.upcase
+  end
+
+  def render_balance(account)
+    number_to_currency(account.balance,
+                       precision: 5,
+                       strip_insignificant_zeros: true)
+  end
+
+  def render_exchange_rate(account)
+    number_with_precision(account.exchange_rate,
+                          precision: 5,
+                          strip_insignificant_zeros: true)
+  end
+
+  def render_balance_in_twd(account)
+    number_to_currency(account.balance_in_twd,
+                       precision: 0)
+  end
+
 end
