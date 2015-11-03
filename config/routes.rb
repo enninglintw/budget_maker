a655e27bb9ab264bc495026ae67b8c98a989ee61
En-ning <playpool513@gmail.com>
2015-11-04 00:00:26 +0800

    add transactions as resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root "admin/accounts#index"
   namespace :admin do
-    resources :accounts
+    resources :accounts do
+      resources :transactions
+    end
+    resources :transactions
   end
 end
