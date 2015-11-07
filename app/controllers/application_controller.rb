87a635dfcbdf67a65428b2693bd79dd73b9c002a
En-ning <playpool513@gmail.com>
2015-11-07 21:36:00 +0800

    fix sidebar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :account_categories, :accounts
+
+  def account_categories
+    ["現金", "儲值", "活存", "信用卡", "投資"]
+  end
+
+  def accounts
+    Account.all.order(:origin_id)
+  end
 end
