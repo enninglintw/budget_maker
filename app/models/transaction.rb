406de1257e8abf43dc72010cd22559ce3a65ec0a
En-ning <playpool513@gmail.com>
2015-11-15 01:52:01 +0800

    refactor model methods

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -47,57 +47,23 @@ class Transaction < ActiveRecord::Base
   end
 
   def transfer?
-    category1 == "轉帳"
-  end
-
-  def transfer
-    amount if transfer?
+    type == "Transfer"
   end
 
   def income?
-    category1 == "收入"
-  end
-
-  def income
-    amount if income?
+    type == "Income"
   end
 
   def expense?
-    category1 == "支出"
+    type == "Expense"
   end
 
-  def expense
-    amount if expense?
-  end
-
-  def category
+  def detail
     if transfer?
-      category1 + "(to #{transfer_account.try(:name)})"
+      type + "(to #{transfer_account.try(:name)})"
     else
-      category1 + "(#{try(:category2)})"
+      type + "(#{try(:category)})"
     end
-  end
-
-  class << self
-
-    def sum_income
-      where(category1: "收入").inject(0) do |sum, transaction|
-        sum + transaction.income
-      end
-    end
-
-    def sum_expense
-      where(category1: "支出").inject(0) do |sum, transaction|
-        sum + transaction.expense
-      end
-    end
-
-    def sum_transfer
-      where(category1: "轉帳").inject(0) do |sum, transaction|
-        sum + transaction.transfer
-      end
-    end
-
   end
 
 end
