cf37ea41d9b641d7e50bd7600fa34f3745b7f11e
En-ning <playpool513@gmail.com>
2015-11-25 03:51:59 +0800

    add counterpart_account_id to transaction

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,17 +2,18 @@
 #
 # Table name: transactions
 #
-#  id             :integer          not null, primary key
-#  account_id     :integer
-#  counterpart_id :integer
-#  origin_id      :integer
-#  date           :date
-#  type           :string(255)
-#  category       :string(255)
-#  note           :text(65535)
-#  amount         :decimal(20, 5)
-#  created_at     :datetime         not null
-#  updated_at     :datetime         not null
+#  id                     :integer          not null, primary key
+#  account_id             :integer
+#  counterpart_id         :integer
+#  counterpart_account_id :integer
+#  origin_id              :integer
+#  date                   :date
+#  type                   :string(255)
+#  category               :string(255)
+#  note                   :text(65535)
+#  amount                 :decimal(20, 5)
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
 #
 
 class Expense < Transaction
